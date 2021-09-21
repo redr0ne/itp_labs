2a8031758830596faf6a2bd9f6ea7e079c90c1ae
redr0ne <73309793+redr0ne@users.noreply.github.com>
2021-09-21 15:50:00 +0300

    calculates circumference

---
+++ lab_4/lab_4_2.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+int main()
+{
+    int d;
+
+    std::cout << "Input diameter: ";
+    std::cin >> d;
+
+
+    float L = 3.14 * d;
+
+    std::cout << "Circumference is: " << L;
+}
