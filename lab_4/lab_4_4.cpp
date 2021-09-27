6185cdb56b1a4b61c5dd41752fa211282df6810a
redr0ne <73309793+redr0ne@users.noreply.github.com>
2021-09-27 23:30:08 +0300

    Add files via upload

---
+++ lab_4/lab_4_4.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <cmath>
+
+int main()
+{
+
+    int a = 0;
+    int b = 0;
+
+    while (a == 0) {
+        std::cout << "\nInput a: ";
+        std::cin >> a;
+
+        if (a == 0) {
+            std::cout << "Zero is not a valid number\n";
+        }
+    }
+
+    while (b == 0) {
+        std::cout << "\nInput b: ";
+        std::cin >> b;
+
+        if (b == 0) {
+            std::cout << "Zero is not a valid number\n";
+        }
+    }
+
+    int a_squared = pow(a, 2);
+    int b_squared = pow(b, 2);
+
+    std::cout << "\nSum of squares is: " << a_squared + b_squared;
+    std::cout << "\nDifference of squares is: " << a_squared - b_squared;
+    std::cout << "\nProduct of squares is: " << a_squared * b_squared;
+    std::cout << "\nQuotient of squares is: " << a_squared / b_squared;
+
+}
