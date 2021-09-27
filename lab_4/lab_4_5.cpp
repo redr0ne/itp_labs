3d3cbf853a82c9c4945d26708d7bd07bc3b8276c
redr0ne <73309793+redr0ne@users.noreply.github.com>
2021-09-27 23:36:59 +0300

    Add files via upload

---
+++ lab_4/lab_4_5.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <cmath>
+
+int main()
+{
+
+    int a = 0;
+    int b = 0;
+
+    while (a == 0) {
+        std::cout << "\nInput a: ";
+        std::cin >> a;
+
+        if (a == 0) {
+            std::cout << "Zero is not a valid number\n";
+        }
+    }
+
+    while (b == 0) {
+        std::cout << "\nInput b: ";
+        std::cin >> b;
+
+        if (b == 0) {
+            std::cout << "Zero is not a valid number\n";
+        }
+    }
+
+    int a_absolute = abs(a);
+    int b_absolute = abs(b);
+
+    std::cout << "\nSum of modules is: " << a_absolute + b_absolute;
+    std::cout << "\nDifference of modules is: " << a_absolute - b_absolute;
+    std::cout << "\nProduct of modules is: " << a_absolute * b_absolute;
+    std::cout << "\nQuotient of modules is: " << a_absolute / b_absolute;
+
+}
