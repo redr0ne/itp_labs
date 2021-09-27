8d1359ad3c0f8819f19ce4c373c5f9e914857e2a
redr0ne <73309793+redr0ne@users.noreply.github.com>
2021-09-27 22:51:08 +0300

    Add files via upload

---
+++ lab_4/lab_4_3.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+int main()
+{
+    int a;
+    int b;
+
+    std::cout << "Input a: ";
+    std::cin >> a;
+
+    std::cout << "Input b: ";
+    std::cin >> b;
+
+
+    float average = (a + b) / 2;
+
+    std::cout << "\nAverage is: " << average;
+}
