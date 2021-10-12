0719b85fba3c0801bdb481a35c39a55a0c1f3542
redr0ne <73309793+redr0ne@users.noreply.github.com>
2021-10-12 15:03:47 +0300

    Update lab_5_2.cpp

--- lab_5/lab_5_2.cpp
+++ lab_5/lab_5_2.cpp
@@ -3,36 +3,24 @@
 
 int main()
 {
-    int A_x;
-    int A_y;
-    int B_x;
-    int B_y;
-    int C_x;
-    int C_y;
+    int a;
+    int b;
+    int c;
 
-    std::cout << "\nInput the x coordinate of point A: ";
-    std::cin >> A_x;
+    std::cout << "\nInput a: ";
+    std::cin >> a;
 
-    std::cout << "\nInput the y coordinate of point A: ";
-    std::cin >> A_y;
+    std::cout << "\nInput b: ";
+    std::cin >> b;
 
-    std::cout << "\nInput the x coordinate of point B: ";
-    std::cin >> B_x;
+    std::cout << "\nInput c: ";
+    std::cin >> c;
 
-    std::cout << "\nInput the y coordinate of point B: ";
-    std::cin >> B_y;
+    int length_ac = abs(a - c);
+    int length_bc = abs(b - c);
+    int sum_of_lengths = length_ac + length_bc;
 
-    std::cout << "\nInput the x coordinate of point C: ";
-    std::cin >> C_x;
-
-    std::cout << "\nInput the y coordinate of point C: ";
-    std::cin >> C_y;
-
-    float length_AC = sqrt(pow(A_x - C_x, 2) + pow(A_y - C_y, 2)); //Calculates length according to the Pythagorean theorem
-    float length_BC = sqrt(pow(B_x - C_x, 2) + pow(B_y - C_y, 2));
-    float sum_of_lengths = length_AC + length_BC;
-
-    std::cout << "\nSection length AC: " << length_AC;
-    std::cout << "\nSection length BC: " << length_BC;
+    std::cout << "\nSection length ac: " << length_ac;
+    std::cout << "\nSection length bc: " << length_bc;
     std::cout << "\nSum of lengths: " << sum_of_lengths;
 }
