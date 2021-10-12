25c8626f0c83c54de8c304dfc6dd8ea3af34cbb5
redr0ne <73309793+redr0ne@users.noreply.github.com>
2021-10-12 15:35:59 +0300

    Update lab_5_3.cpp

--- lab_5/lab_5_3.cpp
+++ lab_5/lab_5_3.cpp
@@ -3,42 +3,31 @@
 
 int main()
 {
-    int A_x;
-    int A_y;
-    int B_x;
-    int B_y;
-    int C_x;
-    int C_y;
+    int a = 0;
+    int b = 1;
+    int c = 2;
 
-	while ((A_x < C_x < B_x) or (A_x > C_x > B_x)) and ((A_y < C_y < B_y) or (A_y > C_y > B_y)){ //Checks if point C is between points A and B
-		
-		std::cout << "\nInput the x coordinate of point A: ";
-		std::cin >> A_x;
+    while (!(a < c < b) and !(a > c > b)) {
 
-		std::cout << "\nInput the y coordinate of point A: ";
-		std::cin >> A_y;
+        std::cout << "\ninput a: ";
+        std::cin >> a;
 
-		std::cout << "\nInput the x coordinate of point B: ";
-		std::cin >> B_x;
+        std::cout << "\ninput b: ";
+        std::cin >> b;
 
-		std::cout << "\nInput the y coordinate of point B: ";
-		std::cin >> B_y;
+        std::cout << "\ninput c: ";
+        std::cin >> c;
 
-		std::cout << "\nInput the x coordinate of point C: ";
-		std::cin >> C_x;
+        if (!(a < c < b) and !(a > c > b)) {
+            std::cout << "\npoint c must be between points a and b";
 
-		std::cout << "\nInput the y coordinate of point C: ";
-		std::cin >> C_y;
-	
-		if !(((A_x < C_x < B_x) or (A_x > C_x > B_x)) and ((A_y < C_y < B_y) or (A_y > C_y > B_y))){
-			std::cout << "\nPoint C must be between points A and B";
-		}
-	
-	}
+        }
 
-    float length_AC = sqrt(pow(A_x - C_x, 2) + pow(A_y - C_y, 2)); //Calculates length according to the Pythagorean theorem
-    float length_BC = sqrt(pow(B_x - C_x, 2) + pow(B_y - C_y, 2));
-    float product_of_lengths = length_AC + length_BC;
+    }
 
-    std::cout << "\nProduct of lengths: " << product_of_lengths;
+    int length_ac = abs(a - c);
+    int length_bc = abs(b - c);
+    int product_of_lengths = length_ac * length_bc;
+
+    std::cout << "\nproduct of lengths: " << product_of_lengths;
 }
